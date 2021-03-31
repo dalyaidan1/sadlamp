f3b76e09b358af3ded75624d19ece63c7f99112d
Aidan Daly <dalyaidan1@gmail.com>
2021-03-31 14:05:37 -0400

    Refactored the JS

--- js/control.js
+++ js/control.js
@@ -1,71 +1,78 @@
 // JavaScript Document
 //----------------------------------------------------------------jquery-----------------------------------------------------------------//
 $(document).ready(function() {
-	var $on = $("#on");
-	var $b1 = $("#b1");
-	var $b2 = $("#b2");
-	var $b3 = $("#b3");
-	var $b4 = $("#b4");
-	var $bgid = $("#bgid");
-	var $bgf = $("#bgf");
-	var $ui = $("#ui");
-	var $hideUI = $("#hideUI");
-
 	
+	/**
+	 * @JSDoc
+	 * 
+	 * changes the bg color to white when the app starts
+	 * changes the start button to exit and vise versa
+	 */
   	$('#on').click(function() {
 		$bgid.css("animation-name", "startup");
-		$on = $("#on");
 		if($on.attr("rows", "1")){
 			$b1.attr("src", "images/brightness_filled_1.png");
 			$b2.attr("src", "images/brightness_empty_2.png");
 			$b3.attr("src", "images/brightness_empty_3.png");
 			$b4.attr("src", "images/brightness_empty_4.png");
 		}
-		var val = ($("#on").attr("value") === "START") ? "EXIT" : "START";
+		let val = ($("#on").attr("value") === "START") ? "EXIT" : "START";
         $(this).attr('value', val);
 		$on.attr("rows","0");
 	});
+
+	/**
+	 * @JSDoc
+	 * 
+	 * @param {jQuery} fillButton the DOM element that was clicked
+	 * @param {int} fillButtonNumber the id order number of the button that was clicked
+	 * @param {string} newBGColor the color that the new bg should be
+	 * 
+	 * Changes the bg color
+	 */
+	function changeBG(fillButton, fillButtonNumber, newBGColor){
+		$("#bgf").css("background-color", newBGColor);
+		let imagePath = "images/brightness_";
+		let count = 1;
+		$("#levels input").each(function(){
+			$(this).attr("src", `${imagePath}empty_${count}.png`);
+			count ++;
+		});
+		fillButton.attr("src", `${imagePath}filled_${fillButtonNumber}.png`);
+	}
 	
-	$b1.click(function() {
-		$bgf.css("background-color", "rgb(255, 255, 255)");
-		$b1.attr("src", "images/brightness_filled_1.png");
-		$b2.attr("src", "images/brightness_empty_2.png");
-		$b3.attr("src", "images/brightness_empty_3.png");
-		$b4.attr("src", "images/brightness_empty_4.png");
+	$("#b1").on("click", function() {
+		changeBG($(this), 1, "rgb(255, 255, 255)")
 	});
-	$b2.click(function() {
-		$bgf.css("background-color", "rgb(238, 238, 238)");
-		$b1.attr("src", "images/brightness_empty_1.png");
-		$b2.attr("src", "images/brightness_filled_2.png");
-		$b3.attr("src", "images/brightness_empty_3.png");
-		$b4.attr("src", "images/brightness_empty_4.png");
+	$("#b2").on("click", function() {
+		changeBG($(this), 2, "rgb(238, 238, 238)")
 	});
-	$b3.click(function() {
-		$bgf.css("background-color", "rgb(221, 221, 221)");
-		$b1.attr("src", "images/brightness_empty_1.png");
-		$b2.attr("src", "images/brightness_empty_2.png");
-		$b3.attr("src", "images/brightness_filled_3.png");
-		$b4.attr("src", "images/brightness_empty_4.png");
+	$("#b3").on("click", function() {
+		changeBG($(this), 3, "rgb(221, 221, 221)")
 	});
-	$b4.click(function() {
-		$bgf.css("background-color", "rgb(204, 204, 204)");
-		$b1.attr("src", "images/brightness_empty_1.png");
-		$b2.attr("src", "images/brightness_empty_2.png");
-		$b3.attr("src", "images/brightness_empty_3.png");
-		$b4.attr("src", "images/brightness_filled_4.png");
+	$("#b4").on("click", function() {
+		changeBG($(this), 4, "rgb(204, 204, 204)")
 	});
 	
-	$bgf.dblclick(function(){
-		$ui.show();
+	// hides the UI
+	$("#bgf").on("dblclick", function(){
+		$("#ui").show();
 	});
-	$hideUI.click(function(){
-		$ui.hide();
+
+	// hides the UI
+	$("#hideUI").on("click", function(){
+		$("#ui").hide();
 	});
 
 });
 
 //------------------------------------------------------------------js------------------------------------------------------------------//
 
+/**
+ * @JSDoc
+ * 
+ * Toggles the current window to fullscreen
+ */
 function toggleFullScreen() {
 	if (!document.fullscreenElement &&
 		!document.mozFullScreenElement && 
